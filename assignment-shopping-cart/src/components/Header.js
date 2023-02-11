4cd87d56604cfb051e81fea54aca011719d58f1c
Sannidhi-Hegde <sannidhi-d.hegde@sixt.com>
2023-02-11 21:17:18 +0530

    header file

--- assignment-shopping-cart/src/components/Header.js
+++ assignment-shopping-cart/src/components/Header.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import '../styles/header.css'
 
 const Header = ({size}) => {
+  const navigate=useNavigate()
+    const routeChange=()=>{
+        let path=`/cart`
+        navigate(path)  
+    }
   return (
     <div>
         <nav className='nav-body'>
             
             <span className='myshop'><h1>Shopping Cart</h1></span>
-            <div className='mycart'>
+            <div className='mycart' onClick={routeChange}>
                 <span><i className='fas fa-cart-plus'></i></span>
                 <sup>{size}</sup>
             </div>
