c3851ef3e1e8dc613984bac57e1a11a32433d800
Sannidhi-Hegde <sannidhi-d.hegde@sixt.com>
2023-02-15 10:32:26 +0530

    Details of each product

--- assignment-shopping-cart/src/components/ProductCard.js
+++ assignment-shopping-cart/src/components/ProductCard.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../styles/products.css'
+import Rating from './Rating'
 
-const ProductCard = ({item}) => {
+const ProductCard = ({item,clickHandler}) => {
+  
     const {id,title,price,description,category,image,rating}=item
     
   return (
@@ -9,16 +11,23 @@ const ProductCard = ({item}) => {
       <div className="image-box">
           <img className='img' src={image} alt="Image" />  
       </div>
+      <br></br>
       <div className="details">
           
           <h4 className='title'>{title}</h4>
-          <p className='price'>${price}</p>
-          <p>{description}</p>
           <p>{category}</p>
-          {/* <p>{rating}</p>     */}
+          <br></br>
+          <Rating value={rating.rate} />
+          <br></br>
+          <h2 className='price'>${price}</h2>
+          <br></br>
+          <p className='description'>{description}</p>
+          <br></br>
+          <h4>Qty:{rating.count}</h4>
       </div>
-      <div>
-        <button className='add-btn'>ADD TO CART</button>
+      <br></br>
+      <div className='btn1'>
+        <button className='add-btn' onClick={()=>clickHandler(item)}>ADD TO CART</button>
       </div>
     
   </div>
