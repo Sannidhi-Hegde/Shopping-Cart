46ebb3aad89386e90a6c65c402b6a03f5cca1cf4
Sannidhi-Hegde <sannidhi-d.hegde@sixt.com>
2023-02-15 20:49:39 +0530

    Header file

--- assignment-shopping-cart/src/components/Header.js
+++ assignment-shopping-cart/src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import '../styles/header.css'
 
+
 const Header = ({size}) => {
   const navigate=useNavigate()
     const routeChange=()=>{
